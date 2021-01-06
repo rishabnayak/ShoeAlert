4bd30901b6aacf84fd16af434ec48e9a80d99e1c
rishabnayak <rishab@bu.edu>
2021-01-05 19:21:32 -0500

    step tracking + notifications for start and stop

--- shoealert/lib/main.dart
+++ shoealert/lib/main.dart
@@ -1,11 +1,28 @@
-import 'dart:async';
-import 'dart:io' show Platform;
+import 'dart:io';
 
-import 'package:beacons_plugin/beacons_plugin.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-Future<void> main() async {
+import 'package:pedometer/pedometer.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+String formatDate(DateTime d) {
+  return d.toString().substring(0, 19);
+}
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('app_icon');
+  final IOSInitializationSettings initializationSettingsIOS =
+      IOSInitializationSettings();
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   runApp(MyApp());
 }
 
@@ -15,12 +32,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _beaconResult = 'Not Scanned Yet.';
-  int _nrMessaggesReceived = 0;
-  var isRunning = false;
-
-  final StreamController<String> beaconEventsController =
-      StreamController<String>.broadcast();
+  Stream<StepCount> _stepCountStream;
+  Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
 
   @override
   void initState() {
@@ -28,61 +42,66 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
-  @override
-  void dispose() {
-    beaconEventsController.close();
-    super.dispose();
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    if (Platform.isAndroid) {
-      //Prominent disclosure
-      await BeaconsPlugin.setDisclosureDialogMessage(
-          title: "Need Location Permission",
-          message: "This app collects location data to work with beacons.");
-
-      //Only in case, you want the dialog to be shown again. By Default, dialog will never be shown if permissions are granted.
-      //await BeaconsPlugin.clearDisclosureDialogShowFlag(false);
+  void onPedestrianStatusChanged(PedestrianStatus event) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails('ShoeAlertChannel', 'ShoeAlert',
+            'Sends Notifications for ShoeAlert');
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    if (event.status == "walking") {
+      await flutterLocalNotificationsPlugin.show(
+          0, 'Walking!', 'You\'re walking!', platformChannelSpecifics);
+    } else if (event.status == "stopped") {
+      await flutterLocalNotificationsPlugin.show(
+          0, 'Stopped!', 'You\'ve stopped!', platformChannelSpecifics);
     }
+    print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
 
-    BeaconsPlugin.listenToBeacons(beaconEventsController);
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
 
-    await BeaconsPlugin.addRegion(
-        "ShoeAlert", "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0");
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
 
-    beaconEventsController.stream.listen(
-        (data) {
-          if (data.isNotEmpty) {
-            setState(() {
-              _beaconResult = data;
-              _nrMessaggesReceived++;
-            });
-            print("Beacons DataReceived: " + data);
-          }
-        },
-        onDone: () {},
-        onError: (error) {
-          print("Error: $error");
-        });
-
-    //Send 'true' to run in background
-    await BeaconsPlugin.runInBackground(true);
-
+  void initPlatformState() async {
     if (Platform.isAndroid) {
-      BeaconsPlugin.channel.setMethodCallHandler((call) async {
-        if (call.method == 'scannerReady') {
-          await BeaconsPlugin.startMonitoring;
-          setState(() {
-            isRunning = true;
-          });
-        }
-      });
-    } else if (Platform.isIOS) {
-      await BeaconsPlugin.startMonitoring;
-      setState(() {
-        isRunning = true;
-      });
+      if (await Permission.activityRecognition.request().isGranted) {
+        _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+        _pedestrianStatusStream
+            .listen(onPedestrianStatusChanged)
+            .onError(onPedestrianStatusError);
+
+        _stepCountStream = Pedometer.stepCountStream;
+        _stepCountStream.listen(onStepCount).onError(onStepCountError);
+      }
+    } else {
+      _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+      _pedestrianStatusStream
+          .listen(onPedestrianStatusChanged)
+          .onError(onPedestrianStatusError);
+
+      _stepCountStream = Pedometer.stepCountStream;
+      _stepCountStream.listen(onStepCount).onError(onStepCountError);
     }
 
     if (!mounted) return;
@@ -91,53 +110,46 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Monitoring Beacons'),
+          title: const Text('ShoeAlert'),
         ),
         body: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('$_beaconResult'),
-              Padding(
-                padding: EdgeInsets.all(10.0),
+              Text(
+                'Steps taken:',
+                style: TextStyle(fontSize: 30),
               ),
-              Text('$_nrMessaggesReceived'),
-              SizedBox(
-                height: 20.0,
+              Text(
+                _steps,
+                style: TextStyle(fontSize: 60),
               ),
-              Visibility(
-                visible: isRunning,
-                child: RaisedButton(
-                  onPressed: () async {
-                    if (Platform.isAndroid) {
-                      await BeaconsPlugin.stopMonitoring;
-
-                      setState(() {
-                        isRunning = false;
-                      });
-                    }
-                  },
-                  child: Text('Stop Scanning', style: TextStyle(fontSize: 20)),
-                ),
+              Divider(
+                height: 100,
+                thickness: 0,
+                color: Colors.white,
               ),
-              SizedBox(
-                height: 20.0,
+              Text(
+                'Pedestrian status:',
+                style: TextStyle(fontSize: 30),
               ),
-              Visibility(
-                visible: !isRunning,
-                child: RaisedButton(
-                  onPressed: () async {
-                    initPlatformState();
-                    await BeaconsPlugin.startMonitoring;
-
-                    setState(() {
-                      isRunning = true;
-                    });
-                  },
-                  child: Text('Start Scanning', style: TextStyle(fontSize: 20)),
+              Icon(
+                _status == 'walking'
+                    ? Icons.directions_walk
+                    : _status == 'stopped'
+                        ? Icons.accessibility_new
+                        : Icons.error,
+                size: 100,
+              ),
+              Center(
+                child: Text(
+                  _status,
+                  style: _status == 'walking' || _status == 'stopped'
+                      ? TextStyle(fontSize: 30)
+                      : TextStyle(fontSize: 20, color: Colors.red),
                 ),
               )
             ],
